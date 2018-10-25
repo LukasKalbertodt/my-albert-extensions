f1a2349247ce26d90611a5ee2b993bfbf9e1567f
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-10-25 19:18:58 +0200

    Improve WinSwitch by showing icons of windows

--- WinSwitch.py
+++ WinSwitch.py
@@ -3,16 +3,76 @@
 """TODO"""
 
 import cgi
+import time
+import os
 from Xlib.display import Display
 from Xlib import X
 from albertv0 import *
 from subprocess import call
+from PIL import Image
 
 __iid__ = "PythonInterface/v0.2"
 __prettyname__ = "WinSwitch"
 __version__ = "1.0"
 __author__ = "Lukas Kalbertodt"
 __trigger__ = "win"
+
+
+def write_png(path, buf, width, height):
+    bs = bytearray()
+
+    for i in buf:
+        bs.append((i >> 16) & 0xFF)
+        bs.append((i >>  8) & 0xFF)
+        bs.append((i >>  0) & 0xFF)
+        bs.append((i >> 24) & 0xFF)
+
+    img = Image.frombytes("RGBA", (width, height), bytes(bs))
+    img.save(path, "png")
+
+def gen_icon(display, id, win):
+    # Create cache directory
+    base_path = f"{cacheLocation()}/WinSwitch/"
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+
+    # Check if we already have a non-old version of the icon
+    path = f"{base_path}/{id}.png"
+    if os.path.exists(path):
+        if os.path.getmtime(path) + 60 * 60 * 12 >= time.time():
+            return path
+
+    # Get icon data. Format is documented here:
+    # https://specifications.freedesktop.org/wm-spec/1.3/ar01s05.html
+    NET_WM_ICON = display.intern_atom('_NET_WM_ICON')
+    res = win.get_full_property(NET_WM_ICON, X.AnyPropertyType)
+    arr = res.value
+
+    # Find biggest icon
+    max_width = 0
+    max_index = 0
+    start = 0
+    while start < len(arr):
+        width = arr[start]
+        height = arr[start + 1]
+
+        if width > max_width:
+            max_width = width
+            max_index = start
+
+        start += width * height + 2
+
+
+    # Write biggest icon
+    width = arr[max_index]
+    height = arr[max_index + 1]
+    length = width * height
+    data = arr[max_index + 2:max_index + 2 + length]
+
+    write_png(path, data, width, height)
+    return path
+
+
 
 def handleQuery(query):
     if not query.isTriggered:
@@ -79,9 +139,13 @@ def handleQuery(query):
                 def activate_win(id):
                     call(['xdotool', 'windowactivate', str(id)])
 
+                # Generate icon
+                icon_path = gen_icon(display, id, win)
+
                 item = Item(id=__prettyname__, completion=query.rawString)
                 item.text = name#cgi.escape(str(type(name)))
-                item.subtext = "Switch <b>to</b>"
+                item.subtext = "Switch to"
+                item.icon = icon_path
                 item.addAction(FuncAction("Switch to", lambda id=id: activate_win(id)))
                 items.append(item)
 
